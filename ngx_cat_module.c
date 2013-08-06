876f80157c44990adcba54b41493666bf908ed49
胡鹏 <hupenghp2005@gmail.com>
2013-08-06 14:35:25 +0800

    add header filter and many time point

--- ngx_cat_module.c
+++ ngx_cat_module.c
@@ -11,7 +11,7 @@
 #include <ngx_http.h>
 
 #define CATPREFIX "X-CAT-"
-
+#define SENDOUTBUFSIZE 400
 
 int pipefd[2]; 
 
@@ -19,10 +19,13 @@ typedef struct {
 	ngx_flag_t          enable;
 } ngx_http_cat_t;
 
-
+unsigned long start_upstream_sec = 0;
+unsigned long start_upstream_msec = 0;
+unsigned int response_start_msec = 0;
 
 static ngx_int_t ngx_cat_filter_init(ngx_conf_t *cf);
 static ngx_int_t ngx_cat_body_filter(ngx_http_request_t *r, ngx_chain_t *chain);
+static ngx_int_t ngx_cat_header_filter(ngx_http_request_t *r);
 static void * ngx_cat_create_conf(ngx_conf_t *cf);
 static char * ngx_cat_merge_conf(ngx_conf_t *cf, void *parent, void *child);
 static char * ngx_http_cat(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
@@ -73,16 +76,37 @@ ngx_module_t  ngx_cat_filter_module = {
 
 
 static ngx_http_output_body_filter_pt  ngx_http_next_body_filter;
+static ngx_http_output_header_filter_pt  ngx_http_next_header_filter;
 
 	static ngx_int_t
 ngx_cat_filter_init(ngx_conf_t *cf)
 {
 	ngx_http_next_body_filter = ngx_http_top_body_filter;
 	ngx_http_top_body_filter = ngx_cat_body_filter;
-	//ngx_http_next_header_filter = ngx_http_top_header_filter;
-	//ngx_http_top_header_filter = ngx_cat_header_filter;
+
+	ngx_http_next_header_filter = ngx_http_top_header_filter;
+	ngx_http_top_header_filter = ngx_cat_header_filter;
 
 	return NGX_OK;
+}
+
+int put_string_to_buf(char* buf, char* key, char* value, int *pos){
+	ngx_memcpy(buf + *pos, key, strlen(key));
+	*pos = *pos + strlen(key);
+	ngx_memcpy(buf + *pos, value, strlen(value));
+	*pos = *pos + strlen(value);
+	return 0;
+}
+
+static ngx_int_t ngx_cat_header_filter(ngx_http_request_t *r){
+	ngx_http_cat_t  *conf;
+	conf = ngx_http_get_module_loc_conf(r, ngx_cat_filter_module);
+	
+	if(conf->enable == 1){
+		response_start_msec = ngx_current_msec;
+	}
+	
+	return ngx_http_next_header_filter(r);
 }
 
 	static ngx_int_t
@@ -93,18 +117,59 @@ ngx_cat_body_filter(ngx_http_request_t *r, ngx_chain_t *chain)
 
 	if(conf->enable == 1){
 		if(r->upstream->state->response_length){
-			char buf[200];
+			char buf[SENDOUTBUFSIZE];
 			int p = 0;
-			memset(buf, 0, 200);
+			memset(buf, 0, SENDOUTBUFSIZE);
 			unsigned int time = ngx_current_msec - (r->start_sec * 1000 + r->start_msec);
-			ngx_memcpy(buf + p, "?request_time=", strlen("?request_time="));
-			p = p + strlen("?request_time=");
-			sprintf(buf + p, "%u", time);
+			char requesttime[40];
+			memset(requesttime, 0, 40);
+			sprintf(requesttime, "%u", time);
+			put_string_to_buf(buf, "?request_time=", requesttime, &p);
+	//		ngx_memcpy(buf + p, "?request_time=", strlen("?request_time="));
+	//		p = p + strlen("?request_time=");
+	//		sprintf(buf + p, "%u", time);
+	//		p = strlen(buf);
+			char upstreamtime[40];
+			memset(upstreamtime, 0, 40);
+			sprintf(upstreamtime, "%lu", r->upstream->state->response_sec * 1000 + r->upstream->state->response_msec);
+			put_string_to_buf(buf, "&upstream_time=", upstreamtime, &p);
+//			ngx_memcpy(buf + p, "&upstream_time=", strlen("&upstream_time="));
+//			p = p + strlen("&upstream_time=");
+//			sprintf(buf + p, "%lu", r->upstream->state->response_sec * 1000 + r->upstream->state->response_msec);
+//			p = strlen(buf);
+			char upstream_start_sec_buf[40];
+			memset(upstream_start_sec_buf, 0, 40);
+			sprintf(upstream_start_sec_buf, "%lu", start_upstream_sec);
+			put_string_to_buf(buf, "&upstream_start_sec=", upstream_start_sec_buf, &p);
+			start_upstream_sec = 0;
+
+			char upstream_start_msec_buf[40];
+			memset(upstream_start_msec_buf, 0, 40);
+			sprintf(upstream_start_msec_buf, "%lu", start_upstream_msec);
+			put_string_to_buf(buf, "&upstream_start_msec=", upstream_start_msec_buf, &p);
+			start_upstream_msec = 0;
+			
+			char response_start_msec_buf[40];
+			memset(response_start_msec_buf, 0, 40);
+			sprintf(response_start_msec_buf, "%u", response_start_msec);
+			put_string_to_buf(buf, "&upstream_response_start_msec=", response_start_msec_buf, &p);
+			response_start_msec = 0;
+			
+			ngx_memcpy(buf + p, "&start_sec=", strlen("&start_sec="));
+			p = p + strlen("&start_sec=");
+			sprintf(buf + p, "%u", (unsigned int)r->start_sec);
 			p = strlen(buf);
-			ngx_memcpy(buf + p, "&upstream_time=", strlen("&upstream_time="));
-			p = p + strlen("&upstream_time=");
-			sprintf(buf + p, "%lu", r->upstream->state->response_sec * 1000 + r->upstream->state->response_msec);
+	
+			ngx_memcpy(buf + p, "&start_msec=", strlen("&start_msec="));
+			p = p + strlen("&start_msec=");
+			sprintf(buf + p, "%u", (unsigned int)r->start_msec);
 			p = strlen(buf);
+			
+			ngx_memcpy(buf + p, "&status=", strlen("&status="));
+			p = p + strlen("&status=");
+			sprintf(buf + p, "%u", (unsigned int)r->headers_out.status);
+			p = strlen(buf);
+			
 			size_t i;
 			for(i = 0; i < r->headers_out.headers.part.nelts; i++){
 				if(!ngx_strncmp(((ngx_table_elt_t*)r->headers_out.headers.part.elts)[i].key.data, CATPREFIX, strlen(CATPREFIX))){
@@ -118,10 +183,14 @@ ngx_cat_body_filter(ngx_http_request_t *r, ngx_chain_t *chain)
 					p = p + ((ngx_table_elt_t*)r->headers_out.headers.part.elts)[i].value.len;
 				}
 			}
-			ngx_memcpy(buf + p, "&status=", strlen("&status="));
-			p = p + strlen("&status=");
-			sprintf(buf + p, "%u", (unsigned int)r->headers_out.status);
-			write(pipefd[1], buf, 200);
+			
+			write(pipefd[1], buf, SENDOUTBUFSIZE);
+		}
+		else{
+			if(!start_upstream_sec){
+				start_upstream_sec = r->upstream->state->response_sec;
+				start_upstream_msec = r->upstream->state->response_msec;
+			}
 		}
 	}
 //	ngx_buf_t    *b;    
